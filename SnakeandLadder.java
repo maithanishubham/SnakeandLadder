a2a820d7f5ed0bf5cee0aa165952d9e1f0187711
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 10:40:40 -0700

    check for option

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -3,14 +3,33 @@ package snakeandladder;
 public class SnakeandLadder {
 		
 		 static  int START_POSITION=0;
+		 static int NO_PLAY =0;
+		    static int IF_LADDER =1;
+		    static int IF_SNAKE =2;
+		    static int Playerposition=0;
 
 		    public static void main(String[] args) {
 
-		    	int dice = (int) (Math.random()*10)%6 +1;
+		    	 System.out.println("Welcome to Snake and ladder game");
+		         int dice = (int) (Math.random()*10)%6 +1;
+		         int option = (int)(Math.random()*10)%3;
+		 	
+		 	    	
+		         if (option==IF_LADDER){
+		             System.out.println("Ladder!!");
+		              Playerposition+= dice;
 
-		        System.out.println("Welcome to Snake and ladder Game");
-		        System.out.println("rolling of the die Number:" + dice);
-		  }
+		         }
+		         else if(option==IF_SNAKE){
+		             System.out.println("snake!!");
+		             Playerposition-=dice;
 
-}
+
+		         }else {
+		             System.out.println("N0 play");
+		         }
+
+		             }                              
+
+		 }
 		    
