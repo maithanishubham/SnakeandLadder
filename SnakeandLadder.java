b14bf314e020f6accf50e66a5bda53eb1c333b6a
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 10:44:45 -0700

    player winning position

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -6,30 +6,35 @@ public class SnakeandLadder {
 		 static int NO_PLAY =0;
 		    static int IF_LADDER =1;
 		    static int IF_SNAKE =2;
-		    static int Playerposition=0;
+		    static int currentposition=0;
 
 		    public static void main(String[] args) {
 
-		    	 System.out.println("Welcome to Snake and ladder game");
-		         int dice = (int) (Math.random()*10)%6 +1;
-		         int option = (int)(Math.random()*10)%3;
-		 	
-		 	    	
-		         if (option==IF_LADDER){
-		             System.out.println("Ladder!!");
-		              Playerposition+= dice;
+		    	System.out.println("Welcome to Snake and ladder game");
+		        while(currentposition<100) {
+		            int dice = (int) (Math.random() * 10) % 6 + 1;
+		            int option = (int) (Math.random() * 10) % 3;
 
-		         }
-		         else if(option==IF_SNAKE){
-		             System.out.println("snake!!");
-		             Playerposition-=dice;
+		            if (option == IF_LADDER) {
+		                System.out.println("ladder!!");
+		                currentposition +=dice;
+
+		            } else if (option == IF_SNAKE) {
+		                System.out.println("snake!!");
+		                currentposition -=dice;
 
 
-		         }else {
-		             System.out.println("N0 play");
-		         }
+		            } else {
+		                System.out.println("N0 play");
+		               
+		            }
+		            if(currentposition<START_POSITION){
+		                currentposition=START_POSITION;
+		            }
+		            
+		            System.out.println("currentposition: "+currentposition);
+		        }
 
-		             }                              
 
+		    }
 		 }
-		    
